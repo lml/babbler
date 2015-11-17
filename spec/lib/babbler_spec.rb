722a9ef5dc1de31a637f4a87f3125b52944dcbb1
Joe Sak <joe@joesak.com>
2015-11-17 15:51:35 -0500

    Update Rspec usage

--- spec/lib/babbler_spec.rb
+++ spec/lib/babbler_spec.rb
@@ -8,36 +8,39 @@ describe Babbler do
     it "should return an appropriate random babble" do
       babble = Babbler.babble
       words = babble.split(" ")
-      words.length.should == 2
-      Babbler.adjectives.should include(words[0])
-      Babbler.nouns.should include(words[1])
+
+      expect(words.length).to be 2
+      expect(Babbler.adjectives).to include(words[0])
+      expect(Babbler.nouns).to include(words[1])
     end
 
     it "should return the same babble given the same seed" do
-      Babbler.babble(1).should == Babbler.babble(1)
+      expect(Babbler.babble(1)).to eq(Babbler.babble(1))
     end
 
     it "should return two adjectives when configured like that" do
       Babbler.configure do |config|
         config.num_adjectives = 2
       end
+
       babble = Babbler.babble
       words = babble.split(" ")
-      words.length.should == 3
-      Babbler.adjectives.should include(words[0], words[1])
-      Babbler.nouns.should include(words[2])
+
+      expect(words.length).to be 3
+      expect(Babbler.adjectives).to include(words[0], words[1])
+      expect(Babbler.nouns).to include(words[2])
     end
 
     it "should return the last combination from safer_words_1" do
       Babbler.config.word_list = :safer_words_1
       allow_any_instance_of(Random).to receive(:rand).and_return(1161, 3038)
-      Babbler.babble().should == "naval apology"
+      expect(Babbler.babble()).to eq("naval apology")
     end
 
     it "should return the last combination from california_words" do
       Babbler.config.word_list = :california_words
       allow_any_instance_of(Random).to receive(:rand).and_return(1128, 942)
-      Babbler.babble().should == "finicky skin"
+      expect(Babbler.babble()).to eq("finicky skin")
     end
   end
 
